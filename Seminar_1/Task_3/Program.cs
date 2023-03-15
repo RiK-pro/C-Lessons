7ea1ed53d05287221484519394a8f83cf48abf0a
RiK <rik_for_all@mail.ru>
2023-03-15 19:42:49 +0600

    added Task_2 and Task_3

---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Проверка числа на четность. Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 0) Console.WriteLine("0 не является четным числом");
+else if (a % 2 == 0)
+    Console.WriteLine($"{a} является четным числом");
+else Console.WriteLine($"{a} не является четным числом");
