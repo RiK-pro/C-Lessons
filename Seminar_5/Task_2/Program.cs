1910802882ab0845020c7c6150e1e20315e0b7e2
RiK <rik_for_all@mail.ru>
2023-03-23 20:26:30 +0600

    Homework for Seminar_5 done

---
+++ Seminar_5/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+void InputArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+        array[i] = new Random().Next(10, 100);
+}
+
+int sum_nechet(int[] array)
+{
+    int sum = 0;
+    for(int i = 0 ; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum_nechet(array)}");
---
+++ Seminar_5/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double find_max(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    double result = 0;
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < min)
+            min = array[i];
+    }
+    result = max - min;
+    return result;
+}
+
+Console.Clear();
+
+double[] array = {3.03, 7.85, 22.13, 2.05, 78.48};
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Разница между максимальным и минимальным: {find_max(array)}");
