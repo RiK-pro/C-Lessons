af03c2d926102f882ef065618f3430ff88342ec4
RiK <rik_for_all@mail.ru>
2023-03-28 15:41:27 +0600

    Homework for Seminar_6 is done

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] InputArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;      
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count += 1;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = InputArray(n);
+Console.Write("Кол-во элементов больше 0: ");
+Console.Write(Count(array));
---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+void Main(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+}
+
+Console.Clear();
+double k1, b1, k2, b2;
+Console.Write("Введите значение k1: ");
+k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+k2 = double.Parse(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+b2 = double.Parse(Console.ReadLine());
+Main(k1, b1, k2, b2);
