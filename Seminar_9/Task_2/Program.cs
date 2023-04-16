b8166f5e7ebcfa77339b089acffd0368233d5009
RiK <rik_for_all@mail.ru>
2023-04-16 14:30:07 +0600

    Homework for Seminar_9 done

---
+++ Seminar_9/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int f(int a, int b)
+{
+    if (a == b)
+        return a;
+    return f(a + 1, b) + a;
+}
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
+
---
+++ Seminar_9/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int Ackerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackerman(m - 1, 1);
+        }
+        else
+        {
+            return Ackerman(m - 1, Ackerman(m, n - 1));
+        }
+    }
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Функция Аккермана: {Ackerman(m, n)}");
